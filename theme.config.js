bbbfbfd266df307ea1b5e6b2a4800f64e54a5d80
Remi Higuchi <remyenar@yahoo.com>
2021-10-12 23:31:24 +0200

    swe

--- theme.config.js
+++ theme.config.js
@@ -6,7 +6,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold md:inline">🇸🇪</span>
       <span className="text-gray-600 font-normal md:inline">
-        🇸Remi
+        Remi
       </span>
     </>
   ),
